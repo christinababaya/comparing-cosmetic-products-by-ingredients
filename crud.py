bca90b09e9232b9ba265861c045bc01b6ed12f68
vagrant <vagrant@vagrant.vm>
2021-05-06 04:43:47 +0000

    server display

--- crud.py
+++ crud.py
@@ -1,7 +1,7 @@
 """CRUD opperations"""
 
 from model import db, Product, Ingredient, Product_Ingredient, Review, connect_to_db
-
+from sqlalchemy import or_
 
 def create_product(name, brand, product_categorization, image=''):
     """Create, add to db, and return a new product."""
@@ -22,13 +22,25 @@ def get_all_products():
 def get_product_by_ingredient(ingredient):
     """gets the ingrgedients"""
 
-    igredient_input = Ingredient.query.filter_by(ingredient_name=ingredient)
-    return Product_Ingredient.query.filter_by(ingredient_id = ingredient_input.ingredient_id)
+    # ingredient_input = Ingredient.query.filter_by(ingredient_name=ingredient)
+    return Product_Ingredient.query.filter_by(ingredient_id=ingredient.ingredient_id)
     # ingredientId = Ingredient.query.filter_by(ingredient=ingredient).get(ingredient_name)
     # productId = Product_Ingredient.query.filter_by(ingredientId=ingredient_id)
     # return Product.query.get(productId)
     #return Product_Ingredient.query.filter_by(ingredient=ingredient.ingredient_name).get()
 
+def search_products_by_substring(substring):
+    """ Find products containing user submitted text. """
+    
+    return Product.query.filter(or_(Product.name.contains(substring),
+                                    Product.brand.contains(substring),
+                                    Product.product_categorization.contains(substring))
+                                ).all()
+
+def search_ingredients_by_substring(substring):
+    """ Find ingredients containing user submitted text. """
+    
+    return Ingredient.query.filter(Ingredient.ingredient_name.contains(substring)).all()
 
 def get_product_by_name(name):
     """Queries db for product by name and retrieves the first entry
@@ -97,6 +109,12 @@ def get_prod_ing_by_prod_and_ing(product, ingredient):
     
     # return Product_Ingredient.query.filter_by(product=product, ingredient=ingredient).first()
     return db.session.query(Product_Ingredient).filter(Product_Ingredient.product_id==product.product_id, Product_Ingredient.ingredient_id==ingredient.ingredient_id).first()
+
+
+def get_ingredients_for_product(product):
+    """ Queries db for all prod_ing rows matching the product. """
+    
+    return Product_Ingredient.query.filter_by(product_id=product.product_id).all()
 
 
 def create_review(product):
--- server.py
+++ server.py
@@ -27,21 +27,36 @@ def allproducts():
     return render_template('all_products.html', products=products)
 
 
-@app.route('/products/<product_name>')
-def show_product(product_name):
+@app.route('/products/<int:product_id>')
+def show_product(product_id):
     """Show details on a particular product."""
 
-    product = crud.get_product_by_name(product_name)
+    product = crud.get_product_id(product_id)
+    product_ingredients = crud.get_ingredients_for_product(product)
 
-    return render_template('product_details.html', product=product)
+    return render_template('product_details.html', product=product,
+                           product_ingredients=product_ingredients)
+    
+@app.route('/products/search', methods=['GET'])
+def search_products():
+    """ Search product database for key term from user. """
+    
+    search_term = request.args.get('product_name')
+    if not search_term:
+        return redirect('/products')
+    
+    products = crud.search_products_by_substring(search_term)
+    
+    return render_template('product-results.html',
+                           search_term=search_term, products=products)
 
-@app.route('/prodingredients')
-def all_product_ingredients():
-    """Show products with same ingredients."""
+# @app.route('/prodingredients')
+# def all_product_ingredients():
+#     """Show products with same ingredients."""
 
-    products = crud.get_product_by_ingredient("Estragole")
+#     products = crud.get_product_by_ingredient("Estragole")
 
-    return render_template('results_page.html', ingredients=products)
+#     return render_template('results_page.html', ingredients=products)
 
 
 @app.route('/ingredients')
@@ -51,19 +66,37 @@ def all_ingedients():
     # ingredients = crud.get_product_by_ingredient("retinol")
     ingredients = crud.get_all_ingredients()
 
-    return render_template('all_product_ingredients.html', ingredients=ingredients)
+    return render_template('all_product_ingredients.html',
+                           ingredients=ingredients)
 
 
-@app.route('/ingredients/<ingredient_id>')
+@app.route('/ingredients/<int:ingredient_id>')
 def show_ingredients(ingredient_id):
-    """Show details on a particular product ingredient."""
+    """Show details on a particular ingredient."""
 
     ingredient = crud.get_ingredient_by_id(ingredient_id)
+    ingredient_products = crud.get_product_by_ingredient(ingredient)
 
-    return render_template('ingredient_details.html', ingredient=ingredient)
+    return render_template('ingredient_details.html',
+                           ingredient=ingredient,
+                           ingredient_products=ingredient_products)
 
 
-
+@app.route('/ingredients/search', methods=['GET'])
+def search_ingredients():
+    """ Search ingredients database for key term from user. """
+    
+    search_term = request.args.get('ingredient_name')
+    if not search_term:
+        return redirect('/ingredients')
+    
+    ingredients = crud.search_ingredients_by_substring(search_term)
+    print('**************')
+    print(ingredients)
+    print('**************')
+    
+    return render_template('ingredient-results.html',
+                           search_term=search_term, ingredients=ingredients)
 
 
 if __name__ == '__main__':
